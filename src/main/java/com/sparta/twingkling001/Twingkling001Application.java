83f09c85a02898f5c8395cc8feaeb4c3ded1b1e8
hyeminLapTop <gpals5987@gmail.com>
2024-06-21 21:33:43 +0900

    first commit

---
+++ src/main/java/com/sparta/twingkling001/Twingkling001Application.java
@@ -0,0 +1,13 @@
+package com.sparta.twingkling001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Twingkling001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Twingkling001Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/twingkling001/Twingkling001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.twingkling001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Twingkling001ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
