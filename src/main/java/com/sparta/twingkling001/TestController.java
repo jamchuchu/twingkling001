6d0cdfb041c4e90a5ce754590807d57e230e4a48
hyeminLapTop <gpals5987@gmail.com>
2024-06-21 22:05:48 +0900

    [refactor] test file 위치 수정

---
+++ src/main/java/com/sparta/twingkling001/TestController.java
@@ -0,0 +1,17 @@
+package com.sparta.twingkling001;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@RequestMapping("/test")
+@Controller
+public class TestController {
+
+    @GetMapping("/helloWorld")
+    @ResponseBody
+    public String Test(){
+        return "HELLO WORLD";
+    }
+}
