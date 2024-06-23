1411dc5cf8b30700238cd6d9eabd0d01c8f51c88
hyeminLapTop <gpals5987@gmail.com>
2024-06-23 17:37:42 +0900

    [refactor] yml 변경 #13

--- src/main/java/com/sparta/twingkling001/config/MailConfig.java
+++ src/main/java/com/sparta/twingkling001/config/MailConfig.java
@@ -24,31 +24,21 @@ public class MailConfig {
     private String username;
     private String password;
     private int port;
+    private Properties properties;
+
 
     @Bean
     public JavaMailSender javaMailService() {
         JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
 //
-
         javaMailSender.setHost("smtp.naver.com");
         javaMailSender.setUsername(username); // 네이버 아이디
         javaMailSender.setPassword(password); // 네이버 비밀번호
 
         log.info(javaMailSender.getPassword());
         javaMailSender.setPort(port); // 메일 인증서버 포트
-        javaMailSender.setJavaMailProperties(getMailProperties()); // 메일 인증서버 정보 가져오기
+        javaMailSender.setJavaMailProperties(properties);
 
         return javaMailSender;
-    }
-
-    private Properties getMailProperties() {
-        Properties properties = new Properties();
-        properties.setProperty("mail.transport.protocol", "smtp"); // 프로토콜 설정
-        properties.setProperty("mail.smtp.auth", "true"); // smtp 인증
-        properties.setProperty("mail.smtp.starttls.enable", "true"); // smtp strattles 사용
-        properties.setProperty("mail.debug", "true"); // 디버그 사용
-        properties.setProperty("mail.smtp.ssl.trust","smtp.naver.com"); // ssl 인증 서버는 smtp.naver.com
-        properties.setProperty("mail.smtp.ssl.enable","true"); // ssl 사용
-        return properties;
     }
 }
