5a19cc9e39ddaccb5d8fdbdf842bb7523a390e47
hyeminLapTop <gpals5987@gmail.com>
2024-06-24 16:15:07 +0900

    [feat] jwt 시도 #14

--- src/main/java/com/sparta/twingkling001/config/SecurityConfig.java
+++ src/main/java/com/sparta/twingkling001/config/SecurityConfig.java
@@ -1,9 +1,9 @@
 package com.sparta.twingkling001.config;
 
 import com.sparta.twingkling001.login.securityLogin.Role;
-import jakarta.servlet.Filter;
-import jakarta.servlet.http.HttpServletRequest;
+import com.sparta.twingkling001.login.token.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,14 +13,16 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.RegexRequestMatcher;
-
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig  {
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -29,31 +31,29 @@ public class SecurityConfig  {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http)  throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)
-                .httpBasic(AbstractHttpConfigurer::disable)
-                .formLogin((formLogin) -> {
-                    formLogin
-                            .loginProcessingUrl("/api/login/success")
-                            .defaultSuccessUrl("/api/login/success")
-                            .failureUrl("/api/login/permit/fail");
-                })
-                .logout((logoutConfig) -> {
-                    logoutConfig
-                            .logoutSuccessUrl("/api/login/permit/logout");
-                })
-
-                .authorizeHttpRequests((authoriationRequests) ->
-                        authoriationRequests
-//                                .anyRequest().permitAll()
-                                .requestMatchers("/", "/login/**", "/api/login/**").permitAll()
-                                .requestMatchers("/users").hasRole(Role.USER.getAuthority())
-                                .requestMatchers("/sellers").hasRole(Role.SELLER.getAuthority())
-                                .requestMatchers(new RegexRequestMatcher("^/api/.*/permit/.*$", null)).permitAll() // 정규 표현식을 사용한 requestMatchers
-                                .anyRequest().authenticated()
-                )
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                .csrf(AbstractHttpConfigurer::disable)
+//                .httpBasic(AbstractHttpConfigurer::disable)
+//                .formLogin((formLogin) -> {
+//                    formLogin
+//                            .loginProcessingUrl("/api/login/success")
+//                            .defaultSuccessUrl("/api/login/success")
+//                            .failureUrl("/api/login/permit/fail");
+//                })
+//                .logout((logoutConfig) -> {
+//                    logoutConfig
+//                            .logoutSuccessUrl("/api/login/permit/logout");
+//                })
+                .authorizeHttpRequests((authorizationRequests) ->
+                        authorizationRequests
+                                .anyRequest().permitAll()
+//                                .requestMatchers("/", "/login/**", "/api/login/**").permitAll()
+//                                .requestMatchers("/users").hasRole(Role.USER.getAuthority())
+//                                .requestMatchers("/sellers").hasRole(Role.SELLER.getAuthority())
+//                                .requestMatchers(new RegexRequestMatcher("^/api/.*/permit/.*$", null)).permitAll()
+//                                .anyRequest().authenticated()
                 );
-        ;
+//                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 }
---
+++ src/main/java/com/sparta/twingkling001/login/token/JwtTokenProvider.java
@@ -0,0 +1,34 @@
+package com.sparta.twingkling001.login.token;
+
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${jwt.expiration}")
+    private long expirationTime;
+
+    public String createToken(Authentication authentication) {
+        String username = authentication.getName();
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + expirationTime);
+
+//        return "";
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.HS512, secretKey)
+                .compact();
+    }
+}
