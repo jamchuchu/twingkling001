fdaf735214e13fc9528b1c9e0af124dc08d33ad8
hyeminLapTop <gpals5987@gmail.com>
2024-06-30 05:36:49 +0900

    [feat] like 작성 #5

--- src/main/java/com/sparta/twingkling001/like/repository/LikeRepository.java
+++ src/main/java/com/sparta/twingkling001/like/repository/LikeRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LikeRepository extends JpaRepository<Like, Long> {
+    Like findLikeByProductIdAndMemberId(Long productId, Long memberId);
 }
